44d0cd6dcf84ac03eda420adbe92d24a591b37ee
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2021-11-28 05:37:38 -0300

    Al hacer click en el carrito redirige a la pagina del componente

--- src/components/Cart/CartWidget.jsx
+++ src/components/Cart/CartWidget.jsx
@@ -1,13 +1,19 @@
-//import React from 'react';
 import Badge from '@mui/material/Badge';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import IconButton from '@mui/material/IconButton';
+import { useNavigate } from "react-router-dom";
 import {useCartContext} from '../../contexts/CartProvider';
 
 function CartWidget(){
+
+  const navigate = useNavigate();
+  function onCartClick(){
+    navigate('/Cart')
+  }
   const  {itemsInCart}  = useCartContext();
     return (
         <IconButton
+        onClick={onCartClick}
         size="large"
         aria-label={itemsInCart.length + " items in cart"}
         color="inherit"
